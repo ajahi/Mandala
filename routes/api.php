67ed90450aaa724502426fbb78480fd3a90283fd
ajahi <himaliamit1@gmail.com>
2022-07-03 05:55:15 +0545

    minor adjustment

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,7 @@ use App\Http\Controllers\PostController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::gett('/',function(){
-    return view
-})
+
 Route::get('/weather',[PostController::class,'index']);
 
 
